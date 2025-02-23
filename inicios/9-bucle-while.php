ae3d7fc96311a8a450504e6ab05ce898643cade5
santiago silva <santiagosblandon@protonmail.com>
2025-02-23 16:51:32 -0500

    update day 23/03/2025

---
+++ inicios/9-bucle-while.php
@@ -0,0 +1,40 @@
+<?php
+
+// BUCLE WHILE
+// Es una estructura de control que itera un conjunto de instrucciones si se cumple una condición
+/*
+$num = 0;
+while($num <= 100) {
+    // Mostrar número actual
+    echo "<h3>$num</h3>";
+    // Aunmentar la variable en cada ciclo
+    $num = $num + 2;
+}
+*/
+
+
+// la funcióm isset(variable) valida si la variable existe y es diferente a null, de lo contrario retornará false
+if(isset($_GET['num'])) {
+    // Se almacena en la variable numero el valor del método GET
+    // Se puede hacer casting para cambiar el tipo de dato de la variable
+    $numero = (int)$_GET['num'];
+}else {
+    $numero = 1;
+}
+
+// Mostrar el título
+echo "<h1>Esta es la tabla de multiplicar del número $numero<h1/>";
+
+$multiplicador = 1;
+
+while($multiplicador <= 10) {
+    // Mostrar tabla de multiplicar
+    echo "$numero x $multiplicador = ".($numero*$multiplicador)."<br/>";
+    // Aumentar multiplicador
+    $multiplicador ++;
+}
+
+// Mostrar los detalles sobre una variable, útil para depurar el código
+var_dump($numero);
+
+?>
